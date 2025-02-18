4e1ebb35da3f80c8866557671a67bd59cceddcd4
Fabien Ferrando <fabien@lajungle.fr>
2025-02-18 21:49:40 +0100

    page header de la home en desktop

--- web/app/themes/AdelineGraphiste9/resources/views/front-page.blade.php
+++ web/app/themes/AdelineGraphiste9/resources/views/front-page.blade.php
@@ -1,6 +1,11 @@
 
 @extends('layouts.app')
-
-@section('content')
+{{--  @php--}}
+{{--      echo '<pre>';--}}
+{{--      var_dump(get_fields($post->id));--}}
+{{--      die();--}}
+{{--  @endphp--}}
 {{--    @include('layouts.flexibles')--}}
+@section('content')
+
 @endsection
--- web/app/themes/AdelineGraphiste9/resources/views/partials/page-header.blade.php
+++ web/app/themes/AdelineGraphiste9/resources/views/partials/page-header.blade.php
@@ -1,3 +1,32 @@
-<div class="page-header">
-  <h1>{!! App::title() !!}</h1>
-</div>
+<section class="page-header">
+
+  <img class="bg-image" src="@asset('images/illu/bg-home.png')" alt="" aria-hidden="true">
+
+  <div class="text-content">
+
+    <h1>{!! App::title() !!}</h1>
+    <p>AdelineGraphiste repose sur des compétences de designer <br>entièrement tournées vers la réalisation de vos rêves.</p>
+    <p>Faisons briller votre marque ensemble !
+    </p>
+
+    <p>Vous avez une idée brillante ? <br>
+      Un projet passionnant ?
+    </p>
+    <p><a href="#" title="Me contacter pour discuter de votre projet">Contactez-moi</a>
+      pour transformer <br>votre
+      vision en réalité <br>visuelle époustouflante !</p>
+
+    <a href="#" class="cta cta-orange" role="button">J'ai un projet !</a>
+
+  </div>
+  <div class="video-content">
+    <img src="@asset('images/illu/mac_home.png')" alt="Mockup d'un écran avec un design graphique">
+
+    <video controls width="100%" muted autoplay title="Vidéo de présentation d'AdelineGraphiste">
+      <source src="@asset('images/videos/adelinegraphiste1080.mp4')" type="video/mp4">
+    </video>
+
+  </div>
+
+</section>
+
