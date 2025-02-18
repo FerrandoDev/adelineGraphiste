d1028ac6fe9b8edd111d8a749e74cb85d7801c48
Fabien Ferrando <fabien@lajungle.fr>
2025-02-18 22:11:09 +0100

    fix svg + font-family nav primary

--- web/app/themes/AdelineGraphiste9/resources/views/components/rs.blade.php
+++ web/app/themes/AdelineGraphiste9/resources/views/components/rs.blade.php
@@ -1,12 +1,12 @@
 <!-- Réseaux sociaux -->
 <div class="social-links" aria-label="Liens vers les réseaux sociaux">
   <a href="https://www.instagram.com/adelinegraphiste" target="_blank" rel="noopener" aria-label="Instagram">
-    <img src="@asset('images/svg/instagram.svg')" alt="Instagram" />
+    <img src="@asset('images/svg/icon/instagram.svg')" alt="Instagram" />
   </a>
   <a href="https://www.facebook.com/adelinegraphiste" target="_blank" rel="noopener" aria-label="Facebook">
-    <img src="@asset('images/svg/facebook.svg')" alt="Facebook" />
+    <img src="@asset('images/svg/icon/facebook.svg')" alt="Facebook" />
   </a>
   <a href="https://www.linkedin.com/in/adelinepichon" target="_blank" rel="noopener" aria-label="Linkedin">
-    <img class="logo-linkedin" src="@asset('images/svg/linkedin-box.svg')" alt="Linkedin" />
+    <img class="logo-linkedin" src="@asset('images/svg/icon/linkedin-box.svg')" alt="Linkedin" />
   </a>
 </div>
